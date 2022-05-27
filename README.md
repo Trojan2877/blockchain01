4f839052f582196d76e90037fa64fce11122209b
Shreyas <shreyas.raviprakash@gmail.com>
2022-05-27 22:23:20 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# blockchain01
-Attemt to build block chain backend in spring
---
+++ src/main/java/com/trojan/blockchain/entity/Block.java
@@ -0,0 +1,26 @@
+package com.trojan.blockchain.entity;
+
+
+public class Block {
+
+	private String block;
+	
+	private String nextBlock;
+
+	public String getBlock() {
+		return block;
+	}
+
+	public void setBlock(String block) {
+		this.block = block;
+	}
+
+	public String getNextBlock() {
+		return nextBlock;
+	}
+
+	public void setNextBlock(String nextBlock) {
+		this.nextBlock = nextBlock;
+	}
+	
+}
